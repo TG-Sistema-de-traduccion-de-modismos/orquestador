67a393f3f5f9fc17843ac46712fec4445e9733d7
pescobarg <pablojaviereg@gmail.com>
2025-10-09 00:09:39 -0500

    Update service URLs in .env and config.py to use the correct IP address

--- app/core/config.py
+++ app/core/config.py
@@ -1,9 +1,9 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    WHISPER_URL: str = "http://100.110.23.5:8002"
-    BETO_URL: str = "http://100.110.23.5:8003"
-    PHI_URL: str = "http://100.110.23.5:8004"
+    WHISPER_URL: str = "http://100.109.110.59:8002"
+    BETO_URL: str = "http://100.109.110.59:8003"
+    PHI_URL: str = "http://100.109.110.59:8004"
     VERSION: str = "2.0"
 
     class Config:
